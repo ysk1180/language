2726fd04cf8d4ef3a0cd5d77371fd8aad0fc84b8
Yusuke Miyazaki <ysk1180@gmail.com>
2019-06-02 09:45:36 +0900

    LanguageStoreの導入

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
-  state = { language: 'english', button: 'red' };
-
-  onLanguageChange = (language, button) => {
-    this.setState({ language, button });
-  };
-
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i className="flag us" onClick={() => this.onLanguageChange('english', 'red')} />
-          <i className="flag nl" onClick={() => this.onLanguageChange('dutch', 'primary')} />
-        </div>
-        <ColorContext.Provider value={this.state.button}>
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="red">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
